55842de04411d031f61fe81a8ade9b57d90c7920
Chris Boldon <cboldon@gmail.com>
2023-05-22 12:30:26 -0700

    Added SCharacter and Setup inputs

--- Source/ActionRoguelike/Public/SCharacter.h
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -30,6 +30,10 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void MoveForward(float Value);
+
+	void Turn(float Value);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
